ffe3438d7a069d509285e07c247b0df6395bdf1a
Mykhailo <dzhammykhailo@knu.ua>
2025-02-06 00:54:22 +0200

    Доробив нсд

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from utils import factorial
-if __name__ == “__main__”:
-    n = input(“Введіть число: ”)
-    print(f”Факторіал {n} = {factorial(n)}\n”)
+from utils import nsd_finder
+if __name__ == "__main__":
+    n = input("Введіть число: ")
+    print(f"Факторіал {n} = {factorial(n)}\n")
+if __name__ == "__main__":
+    a, b = [int(el) for el in input().split()]
+    print(f"НСД({a},{b}) = {nsd_finder(a, b)}")
