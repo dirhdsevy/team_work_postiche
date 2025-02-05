f691416822267ec3d0a19a667659b5fa3775b705
loweffortmisery <189833560+loweffortmisery@users.noreply.github.com>
2025-02-06 01:17:39 +0200

    added calling of is_power_of_5() in main.py

--- main.py
+++ main.py
@@ -6,3 +6,9 @@ if __name__ == "__main__":
 if __name__ == "__main__":
     a, b = [int(el) for el in input().split()]
     print(f"НСД({a},{b}) = {nsd_finder(a, b)}")
+
+
+from utils import is_power_of_5
+if __name__ == "__main__":
+	n = int(input("Введіть число для перевірки:"))
+	print(f"{n} {"не "*(not(is_power_of_5(n)))}є степенем п'ятірки")
